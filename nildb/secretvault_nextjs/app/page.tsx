2d0c3e42291abbea4772375e2311145a0fdb54da
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2025-01-14 13:16:36 +0100

    chore: init repo

---
+++ nildb/secretvault_nextjs/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  // TODO:
+  // Add form (create-credentials)
+  // Add form (get-credentials)
+  return (
+    <main className='p-8 h-screen w-screen flex items-center justify-center'>
+      <h1 className='text-2xl font-bold'>Secure Credential Manager</h1>
+    </main>
+  );
+}
