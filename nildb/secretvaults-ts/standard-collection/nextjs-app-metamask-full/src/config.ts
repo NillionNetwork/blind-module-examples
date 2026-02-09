0040771c02fa458fde5bc4cf53183d362bf364af
psofiterol <gpentafragkas@protonmail.com>
2026-02-09 14:56:13 +0200

    chore: remove nilchain from MM config

--- nildb/secretvaults-ts/standard-collection/nextjs-app-metamask-full/src/config.ts
+++ nildb/secretvaults-ts/standard-collection/nextjs-app-metamask-full/src/config.ts
@@ -2,7 +2,6 @@
 export const NETWORK_CONFIG = {
     chainId: "nillion-chain-testnet-1",
     nilauthChainId: 11155111,
-    nilchain: "http://rpc.testnet.nilchain-rpc-proxy.nilogy.xyz",
     nilauth: "https://nilauth-1bc3.staging.nillion.network",
     nildb: [
       "https://nildb-stg-n1.nillion.network",
